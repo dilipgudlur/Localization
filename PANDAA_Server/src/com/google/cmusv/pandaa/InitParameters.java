c428b80c1d4fef3b2c0180663a19b2bac714df44
dilipgudlur <dilip.gudlur@sv.cmu.edu>
2011-09-28 01:56:31 -0700

    removing cached directories

--- PANDAA_Server/src/com/google/cmusv/pandaa/InitParameters.java
+++
@@ -1,7 +0,0 @@
-package com.google.cmusv.pandaa;
-/*Class Handles manages specific settings like TDOA parameters,threshold computation,network 
- * ports,sound event details, time sync details etc  */
-
-public class InitParameters { //static implementation - just set the parameters here
-
-}
