20863fa25cf390ec872e0d12664a2353ee30d299
Trevor Pering <grafnu@gmail.com>
2011-10-06 07:41:55 -0700

    Adding device ID field

--- src/edu/cmu/pandaa/shared/stream/GenericFrame.java
+++ src/edu/cmu/pandaa/shared/stream/GenericFrame.java
@@ -15,6 +15,7 @@ public class GenericFrame {
 	}
 
 	public static class Header implements Serializable {
+	    String id; // device ID (hostname, IP address, whatever)
 		public long startTime; // client start time, ala System.currentTimeMillis()
 		public long frameTime; // duration of each frame, measured in ms
 	}
