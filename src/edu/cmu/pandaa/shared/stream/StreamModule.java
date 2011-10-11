c3dcd18f39f53d68f3576c502f9ef6f04b98a4e3
Trevor Pering <grafnu@gmail.com>
2011-10-11 16:57:36 -0700

    Adding example of using StreamModule

--- src/edu/cmu/pandaa/shared/stream/StreamModule.java
+++ src/edu/cmu/pandaa/shared/stream/StreamModule.java
@@ -1,24 +1,9 @@
-
+package edu.cmu.pandaa.shared.stream;
+import edu.cmu.pandaa.shared.stream.header.StreamHeader;
+import edu.cmu.pandaa.shared.stream.header.StreamHeader.StreamFrame;
 
 interface StreamModule {
-    StreamHeader initialize(StreamHeader inHeader);
-    StreamFrame process(StreamFrame inFrame);
-    void close();
-}
-
-public void go(FrameStream in, FrameStream out, StreamModule m1, StreamModule m2) {
-
-    StreamHeader header = in.getHeader();
-    header = m1.initialize(header);
-    header = m2.initialize(header);
-    out.setHeader(header);
-
-    while ((StreamFrame frame = in.recvFrame()) != null) {
-	frame = m1.process(frame);
-	frame = m2.process(frame);
-	out.sendFrame(frame);
-    }
-
-    m1.close();
-    m2.close();
+  StreamHeader init(StreamHeader inHeader);
+  StreamFrame process(StreamFrame inFrame);
+  void close();
 }
