fad6bd7683878d0ae422dbb0a926bfb84dce193b
Divya Vavili <divyavavili@cmu.edu>
2011-10-14 08:04:15 -0700

    Wav file testing updated

--- src/edu/cmu/pandaa/client/desktop/PandaaApplnTester.java
+++ src/edu/cmu/pandaa/client/desktop/PandaaApplnTester.java
@@ -160,13 +160,11 @@ public class PandaaApplnTester extends JPanel implements ActionListener {
 	// ByteArrayOutputStream object.
 	private void captureAudio() {
 		try {
-			// Get and display a list of
-			// available mixers.
 			Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();
 			System.out.println("Available mixers:");
 			for (int cnt = 0; cnt < mixerInfo.length; cnt++) {
 				System.out.println(mixerInfo[cnt].getName());
-			}// end for loop
+			}
 
 			// Get everything set up for capture
 			// audioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
@@ -177,17 +175,13 @@ public class PandaaApplnTester extends JPanel implements ActionListener {
 			DataLine.Info dataLineInfo = new DataLine.Info(
 					TargetDataLine.class, audioFormat);
 
-			System.out.println(" AKTUALNY => " + mixerInfo[2].getName());
 			Mixer mixer = AudioSystem.getMixer(mixerInfo[2]);
 
-			// Get a TargetDataLine on the selected
-			// mixer.
 			targetDataLine = (TargetDataLine) mixer.getLine(dataLineInfo);
-			// Prepare the line for use.
 			targetDataLine.open(audioFormat);
 			targetDataLine.start();
 
-			// Create a thread to capture the microphone
+			// Creating a thread to capture the microphone
 			// data and start it running. It will run
 			// until the Stop button is clicked.
 			Thread captureThread = new CaptureThread();
@@ -195,19 +189,14 @@ public class PandaaApplnTester extends JPanel implements ActionListener {
 		} catch (Exception e) {
 			System.out.println(e);
 			System.exit(0);
-		}// end catch
-	}// end captureAudio method
+		}
+	}
 
 	// This method plays back the audio data that
 	// has been saved in the ByteArrayOutputStream
 	private void playAudio() {
 		try {
-			// Get everything set up for playback.
-			// Get the previously-saved data into a byte
-			// array object.
 			byte audioData[] = byteArrayOutputStream.toByteArray();
-			// Get an input stream on the byte array
-			// containing the data
 			InputStream byteArrayInputStream = new ByteArrayInputStream(
 					audioData);
 			AudioFormat audioFormat = getAudioFormat();
@@ -227,61 +216,43 @@ public class PandaaApplnTester extends JPanel implements ActionListener {
 		} catch (Exception e) {
 			System.out.println(e);
 			System.exit(0);
-		}// end catch
-	}// end playAudio
+		}
+	}
 
-	// This method creates and returns an
-	// AudioFormat object for a given set of format
-	// parameters. If these parameters don't work
-	// well for you, try some of the other
-	// allowable parameter values, which are shown
-	// in comments following the declartions.
+	
 	private AudioFormat getAudioFormat() {
 		float sampleRate = 8000.0F;
-		// 8000,11025,16000,22050,44100
 		int sampleSizeInBits = 16;
-		// 8,16
 		int channels = 1;
-		// 1,2
 		boolean signed = true;
-		// true,false
 		boolean bigEndian = false;
-		// true,false
 		return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed,
 				bigEndian);
-	}// end getAudioFormat
+	}
 
 	// Inner class to capture data from microphone
 	class CaptureThread extends Thread {
-		// An arbitrary-size temporary holding buffer
 		byte tempBuffer[] = new byte[10000];
 
 		public void run() {
 			byteArrayOutputStream = new ByteArrayOutputStream();
 			stopCapture = false;
-			try {// Loop until stopCapture is set by
-					// another thread that services the Stop
-					// button.
+			try {
 				while (!stopCapture) {
-					// Read data from the internal buffer of
-					// the data line.
+				
 					int cnt = targetDataLine.read(tempBuffer, 0,
 							tempBuffer.length);
 					if (cnt > 0) {
-						// Save data in output stream object.
 						byteArrayOutputStream.write(tempBuffer, 0, cnt);
-					}// end if
-				}// end while
+					}
+				}
 				byteArrayOutputStream.close();
 			} catch (Exception e) {
 				System.out.println(e);
 				System.exit(0);
-			}// end catch
-		}// end run
-	}// end inner class CaptureThread
-		// ===================================//
-		// Inner class to play back the data
-		// that was saved.
+			}
+		}
+	}
 
 	class PlayThread extends Thread {
 		byte tempBuffer[] = new byte[10000];
@@ -289,27 +260,20 @@ public class PandaaApplnTester extends JPanel implements ActionListener {
 		public void run() {
 			try {
 				int cnt;
-				// Keep looping until the input read method
-				// returns -1 for empty stream.
 				while ((cnt = audioInputStream.read(tempBuffer, 0,
 						tempBuffer.length)) != -1) {
 					if (cnt > 0) {
-						// Write data to the internal buffer of
-						// the data line where it will be
-						// delivered to the speaker.
 						sourceDataLine.write(tempBuffer, 0, cnt);
-					}// end if
-				}// end while
-					// Block and wait for internal buffer of the
-					// data line to empty.
+					}
+				}
 				sourceDataLine.drain();
 				sourceDataLine.close();
 			} catch (Exception e) {
 				System.out.println(e);
 				System.exit(0);
-			}// end catch
-		}// end run
-	}// end inner class PlayThread
+			}
+		}
+	}
 	
 	public static final int byteArrayToInt(byte [] b, boolean bigEndian) {
 		int x = 0;
