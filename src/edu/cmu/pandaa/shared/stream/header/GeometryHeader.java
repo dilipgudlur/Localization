76ca7a73682f45b280f764e68f6942003aac9e54
dilipgudlur <dilip.gudlur@sv.cmu.edu>
2011-10-17 00:59:27 -0700

    added ProcessGeometryModule.java,GeometryFileStream.java, modified GeometryHeader.java

--- src/edu/cmu/pandaa/shared/stream/header/GeometryHeader.java
+++ src/edu/cmu/pandaa/shared/stream/header/GeometryHeader.java
@@ -2,14 +2,34 @@ package edu.cmu.pandaa.shared.stream.header;
 
 import java.io.Serializable;
 
+import edu.cmu.pandaa.shared.stream.header.FeatureHeader.FeatureFrame;
+
 public class GeometryHeader extends StreamHeader implements Serializable {
-	String[] deviceIds;
+	public String[] deviceIds;
 
   public GeometryHeader(String id, long startTime, int frameTime) {
     super(id, startTime, frameTime);
   }
 
 	public class GeometryFrame extends StreamFrame implements Serializable {
-		double[][] geometry;  
+		public double[][] geometry;
+		
+		public GeometryFrame(int seq, double[][] geometry) {
+		      super(seq);
+		      this.geometry = geometry;
+		}
+
+	    public GeometryFrame(double[][] geometry) {
+	      this.geometry = geometry;      
+	    }
 	}
+	
+	public GeometryFrame makeFrame(double[][] geometry) {
+	    return new GeometryFrame(geometry);
+	  }
+
+	public GeometryFrame makeFrame(int seq, double[][] geometry) {
+		return new GeometryFrame(seq, geometry);
+	}
+
 }
