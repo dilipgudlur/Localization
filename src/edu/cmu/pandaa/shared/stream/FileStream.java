f09bf7bace5761cca84ed38d5d33c483a84cc4e9
Divya Vavili <divyavavili@cmu.edu>
2011-10-19 10:05:53 -0700

    "File Stream change"

--- src/edu/cmu/pandaa/shared/stream/FileStream.java
+++ src/edu/cmu/pandaa/shared/stream/FileStream.java
@@ -1,5 +1,6 @@
 package edu.cmu.pandaa.shared.stream;
 
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -14,83 +15,98 @@ import edu.cmu.pandaa.shared.stream.header.StreamHeader.StreamFrame;
 
 public class FileStream implements FrameStream {
 
-  private File audioFile;
-  private static int sequenceNum;
-  ObjectOutputStream oos = null;
-  ObjectInputStream ois = null;
-  
-  public FileStream(String fileName) {
-    audioFile = new File(fileName);
-    sequenceNum = 0;
-    // Open output stream
-    if (this.audioFile == null) {
-      throw new IllegalStateException("fileName is null");
-    }
+	private File audioFile;
+	private static int sequenceNum;
+	ObjectOutputStream oos = null;
+	ObjectInputStream ois = null;
 
-    try {
-      oos = new ObjectOutputStream(new FileOutputStream(this.audioFile));
-      ois = new ObjectInputStream(new FileInputStream(this.audioFile));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-  
-  public synchronized void setHeader(StreamHeader h) {
-    try {
-      oos.writeObject(h);
-      oos.flush();
-    } catch (FileNotFoundException e) {
-      throw new IllegalStateException("Cannot Open File", e);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+	public FileStream(String fileName) {
+		audioFile = new File(fileName);
+		sequenceNum = 0;
+		// Open output stream
+		if (this.audioFile == null) {
+			throw new IllegalStateException("fileName is null");
+		}
 
-  public synchronized StreamHeader getHeader() {
-    StreamHeader audioHeader = null;
-    try {
-      audioHeader = (StreamHeader) ois.readObject();
-    } catch (OptionalDataException e) {
-      e.printStackTrace();
-    } catch (ClassNotFoundException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    return audioHeader;
-  }
+		try {
+			oos = new ObjectOutputStream(new FileOutputStream(this.audioFile));
+			ois = new ObjectInputStream(new FileInputStream(this.audioFile));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-  public synchronized void sendFrame(StreamFrame m) throws IllegalStateException {
-    try {
-      m.seqNum = sequenceNum++;
-            oos.writeObject(m);
-            oos.flush();
-    } catch (FileNotFoundException e) {
-      throw new IllegalStateException("Cannot Open File", e);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+	public synchronized void setHeader(StreamHeader h) {
+		try {
+			oos.writeObject(h);
+			oos.flush();
+		} catch (FileNotFoundException e) {
+			throw new IllegalStateException("Cannot Open File", e);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-  public StreamFrame recvFrame() {
-    StreamFrame audioFrame = null;
-    try {
-      audioFrame = (StreamFrame) ois.readObject();
-    } catch (OptionalDataException e) {
-      e.printStackTrace();
-    } catch (ClassNotFoundException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    return audioFrame;
-  }
-  
-  public void setFile(File audioFile) {
-    this.audioFile = audioFile;
-  }
+	public synchronized StreamHeader getHeader() {
+		StreamHeader audioHeader = null;
+		try {
+			audioHeader = (StreamHeader) ois.readObject();
+		} catch (EOFException e) {
+			
+		} catch (OptionalDataException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return audioHeader;
+	}
 
-  public File getFile() {
-    return audioFile;
-  }
+	public synchronized void sendFrame(StreamFrame m)
+			throws IllegalStateException {
+		try {
+			m.seqNum = sequenceNum++;
+			oos.writeObject(m);
+			oos.flush();
+		} catch (FileNotFoundException e) {
+			throw new IllegalStateException("Cannot Open File", e);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void close() {
+		try {
+			ois.close();
+			oos.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public StreamFrame recvFrame() {
+		StreamFrame audioFrame = null;
+		try {
+			audioFrame = (StreamFrame) ois.readObject();
+		} catch (EOFException e) {
+			
+		} catch (OptionalDataException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return audioFrame;
+	}
+
+	public void setFile(File audioFile) {
+		this.audioFile = audioFile;
+	}
+
+	public File getFile() {
+		return audioFile;
+	}
 }
