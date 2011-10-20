4d6804fb71e52b05fe3e0bcd621ae7ca0ab13ed6
Divya Vavili <divyavavili@cmu.edu>
2011-10-20 13:00:48 -0700

    Wav conversion with UI - desktop/AudioConversionUI

--- src/edu/cmu/pandaa/header/RawAudioHeader.java
+++ src/edu/cmu/pandaa/header/RawAudioHeader.java
@@ -33,13 +33,15 @@ import java.io.Serializable;
 
 public class RawAudioHeader extends StreamHeader implements Serializable {
 
-	Long samplingRate;
-	Integer numChannels;
-	Integer audioFormat;
-	Integer bitsPerSample;
-	Long subChunk2Size;
+	long samplingRate;
+	long numChannels;
+	int audioFormat;
+	int bitsPerSample;
+	long subChunk2Size;
 	
-	public RawAudioHeader(long startTime, int frameTime, int audioFormat, int numChannels, long samplingRate, int bitsPerSample, long subChunk2Size) {
+	public static final int DEAFULT_FRAMETIME = 100;
+	
+	public RawAudioHeader(long startTime, int frameTime, int audioFormat, long numChannels, long samplingRate, int bitsPerSample, long subChunk2Size) {
 		super("", startTime, frameTime);
 		this.samplingRate = samplingRate;
 		this.numChannels = numChannels;
@@ -52,7 +54,7 @@ public class RawAudioHeader extends StreamHeader implements Serializable {
 		return samplingRate;
 	}
 
-	public int getNumChannels() {
+	public long getNumChannels() {
 		return numChannels;
 	}
 
