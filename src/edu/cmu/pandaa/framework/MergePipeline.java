3d3b99f4ed693dc491cc2a8ebf15ab3d8ff01af4
Trevor Pering <grafnu@gmail.com>
2011-10-20 23:00:03 -0700

    Adding the merge pipeline

--- src/edu/cmu/pandaa/framework/MergePipeline.java
+++ src/edu/cmu/pandaa/framework/MergePipeline.java
@@ -1,9 +1,6 @@
 package edu.cmu.pandaa.framework;
 
-import edu.cmu.pandaa.header.DistanceHeader;
-import edu.cmu.pandaa.header.ImpulseHeader;
-import edu.cmu.pandaa.header.MultiHeader;
-import edu.cmu.pandaa.header.StreamHeader;
+import edu.cmu.pandaa.header.*;
 import edu.cmu.pandaa.header.StreamHeader.StreamFrame;
 import edu.cmu.pandaa.module.DummyModule;
 import edu.cmu.pandaa.module.StreamModule;
@@ -16,22 +13,40 @@ import edu.cmu.pandaa.module.StreamModule;
  */
 
 public class MergePipeline implements StreamModule {
+  final long now = System.currentTimeMillis();
+  final int frameTime = 100;
+  String[] dummyIds = { "a", "b", "c" };
+
+  /* First step is to take in a multiFrame consisting of impulseframes and turn it into time differences */
+  StreamModule geometry = new DummyModule(new GeometryHeader(dummyIds, now, frameTime));
+
+  /* Then we consolidate bunches of impulse frames into larger consolidated frames for processing */
+  StreamModule smooth = new DummyModule();
 
   @Override
   public StreamHeader init(StreamHeader inHeader) {
-    MultiHeader mh = (MultiHeader) inHeader;
-    return mh;
+    MultiHeader multiHeader = (MultiHeader) inHeader;
+    if (!(multiHeader.getOne() instanceof DistanceHeader)) {
+       throw new IllegalArgumentException("Merge pipe multiheader should contain ImpulseHeaders");
+    }
+    StreamHeader header = geometry.init(inHeader);
+    header = smooth.init(header);
+    if (!(header instanceof GeometryHeader)) {
+      throw new IllegalArgumentException("Output should be GeometryHeader");
+    }
+    return header;
   }
 
   @Override
-  public StreamHeader.StreamFrame process(StreamFrame inFrame) {
+  public StreamFrame process(StreamFrame inFrame) {
     if (inFrame == null) {
       return null;
     }
-    return inFrame;
+    StreamFrame frame = geometry.process(inFrame);
+    frame = smooth.process(frame);
+    return frame;
   }
 
   @Override
   public void close() {
-  }
-}
+  }}
