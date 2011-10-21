a1bb264cf35cc5e8d2de26e3581c26c41a417a1d
Trevor Pering <grafnu@gmail.com>
2011-10-21 13:35:10 -0700

    Changing order of arguments for main test programs

--- test_all.sh
+++ test_all.sh
@@ -4,10 +4,16 @@ OPTS="-classpath $CLASSPATH"
 PACKAGE=edu.cmu.pandaa
 
 cd test/
-java $OPTS $PACKAGE.module.FeatureExtractTest sample_input1.wav impulses1.txt
-java $OPTS $PACKAGE.module.FeatureExtractTest sample_input2.wav impulses2.txt
-java $OPTS $PACKAGE.module.FeatureExtractTest sample_input3.wav impulses3.txt
-java $OPTS $PACKAGE.module.TDOAImpulseCorrelationModule impulses1.txt impulses2.txt tdoa12.txt
-java $OPTS $PACKAGE.module.TDOAImpulseCorrelationModule impulses2.txt impulses2.txt tdoa23.txt
-java $OPTS $PACKAGE.module.TDOAImpulseCorrelationModule impulses1.txt impulses3.txt tdoa13.txt
-java $OPTS $PACKAGE.module.ProcessGeometryModule tdoa12.txt tdoa23.txt tdoa13.txt geometry.txt
+
+# Format for all main options is:
+#
+# java ... (options) output_file input_file(s)
+#
+
+java $OPTS $PACKAGE.module.FeatureExtractTest impulses-1.txt sample_input-1.wav
+java $OPTS $PACKAGE.module.FeatureExtractTest impulses-2.txt sample_input-2.wav
+java $OPTS $PACKAGE.module.FeatureExtractTest impulses-3.txt sample_input-3.wav
+java $OPTS $PACKAGE.module.TDOAImpulseCorrelationModule tdoa-12.txt impulses-1.txt impulses-2.txt
+java $OPTS $PACKAGE.module.TDOAImpulseCorrelationModule tdoa-13.txt impulses-1.txt impulses-3.txt
+java $OPTS $PACKAGE.module.TDOAImpulseCorrelationModule tdoa-23.txt impulses-2.txt impulses-3.txt
+java $OPTS $PACKAGE.module.ProcessGeometryModule geometry.txt tdoa-12.txt tdoa-23.txt tdoa-13.txt
