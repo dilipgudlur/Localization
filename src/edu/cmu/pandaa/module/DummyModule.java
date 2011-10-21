96c1f6a3bb5ae6d050fd6637789990d01c6ea7e5
Trevor Pering <grafnu@gmail.com>
2011-10-20 22:42:25 -0700

    Adding dummy module!

---
+++ src/edu/cmu/pandaa/module/DummyModule.java
@@ -0,0 +1,36 @@
+package edu.cmu.pandaa.module;
+
+
+import edu.cmu.pandaa.header.StreamHeader;
+import edu.cmu.pandaa.header.StreamHeader.StreamFrame;
+import org.omg.PortableInterceptor.ORBInitInfoPackage.InvalidNameHelper;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: peringknife
+ * Date: 10/20/11
+ * Time: 7:05 PM
+ */
+
+public class DummyModule implements StreamModule {
+  final StreamHeader header;
+
+  public DummyModule() {
+    this.header = null;
+  }
+
+  public DummyModule(StreamHeader header) {
+    this.header = header;
+  }
+
+  public StreamHeader init(StreamHeader inHeader) {
+    return header == null ? inHeader : header;
+  }
+
+  public StreamFrame process(StreamFrame inFrame) {
+    return header == null ? inFrame : header.makeFrame();
+  }
+
+  public void close() {
+  }
+}
