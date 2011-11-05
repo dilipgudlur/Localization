5b9045a5f525cad41a9333e95a2148504f017e09
Qi Zheng <zqsharejoy@gmail.com>
2011-11-05 00:51:07 -0700

    unit test for impulseStreamModule

--- src/edu/cmu/pandaa/module/ImpulseStreamModule.java
+++ src/edu/cmu/pandaa/module/ImpulseStreamModule.java
@@ -11,7 +11,7 @@ import edu.cmu.pandaa.stream.ImpulseFileStream;
 import edu.cmu.pandaa.stream.RawAudioFileStream;
 
 public class ImpulseStreamModule implements StreamModule {
-	private double max = 0.00305;
+	private double max = 1.85;
 	private int sampleRate = 16000;
 	int nsPerSample = 10 ^ 9 / sampleRate; // nanosecond per sample
 	private double threshold = max / 2; // threshold for amplitude
@@ -23,46 +23,30 @@ public class ImpulseStreamModule implements StreamModule {
 	}
 
 	public static void main(String[] args) throws Exception {
-		try {
-			ImpulseStreamModule ism = new ImpulseStreamModule();
-			ism.run("test/sample_input-1.wav", "test/impulses-1.txt");
-		} catch (Exception e) {
-			e.printStackTrace();
+		
+		int arg = 0;
+	    String impulseFilename = args[arg++];
+	    String audioFilename = args[arg++];
+	    if (args.length > arg || args.length < arg) {
+	      throw new IllegalArgumentException("Invalid number of arguments");
+	    }
+
+	    System.out.println("Extract impulses: " + audioFilename + " to " + impulseFilename);
+		
+		ImpulseFileStream foo = new ImpulseFileStream(impulseFilename, true);
+		RawAudioFileStream rfs = new RawAudioFileStream(audioFilename);
+		ImpulseStreamModule ism = new ImpulseStreamModule();
+
+		RawAudioHeader header = (RawAudioHeader) rfs.getHeader();
+		ImpulseHeader iHeader = (ImpulseHeader) ism.init(header);
+		foo.setHeader(iHeader);
+
+		RawAudioFrame audioFrame = null;
+		while ((audioFrame = (RawAudioFrame) rfs.recvFrame()) != null) {
+			StreamFrame streamFrame = ism.process(audioFrame);
+			foo.sendFrame(streamFrame);
 		}
-	}
-
-	/*
-	 * Example2: A run method that could be used to create a new thread to test
-	 * just this class
-	 */
-	public void run(String audioFilename, String impulseFilename) {
-		try {
-
-			ImpulseFileStream foo = new ImpulseFileStream(impulseFilename, true);
-
-			RawAudioFileStream rfs = new RawAudioFileStream(audioFilename);
-
-			ImpulseStreamModule ism = new ImpulseStreamModule();
-
-			RawAudioHeader header = (RawAudioHeader) rfs.getHeader();
-			ImpulseHeader iHeader = (ImpulseHeader) ism.init(header);
-			foo.setHeader(iHeader);
-
-			RawAudioFrame audioFrame = null;
-			while ((audioFrame = (RawAudioFrame) rfs.recvFrame()) != null) {
-				StreamFrame streamFrame = ism.process(audioFrame);
-				foo.sendFrame(streamFrame);
-			}
-
-			/*
-			 * ImpulseHeader header2 = foo.getHeader(); ImpulseFrame frame2 =
-			 * foo.recvFrame(); frame2 = foo.recvFrame();
-			 */
-			foo.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		foo.close();
 	}
 
 	@Override
@@ -86,14 +70,12 @@ public class ImpulseStreamModule implements StreamModule {
 		int index = 0;
 		short[] frame = ((RawAudioFrame) inFrame).getAudioData();
 		int peakNum = maxHeight(frame, 0, frame.length);
-		short[] peakMagnitudes = new short[1];
-		int[] peakOffsets = new int[1];
-    peakMagnitudes[0] = frame[0];
-    peakOffsets[0] = peakNum * nsPerSample;
-    /*
+		short[] peakMagnitudes = new short[peakNum];
+		int[] peakOffsets = new int[peakNum];
+    
 		if (peakNum > 0) {
 			for (int i = 0; i < frame.length; i++) {
-				double value = java.lang.Math.abs((double) frame[i]) / 65535.0;
+				double value = java.lang.Math.abs((double) frame[i]) / 32768.0;
 				if (value > threshold) {
 					peakMagnitudes[index] = frame[i];
 					peakOffsets[index] = (inFrame.seqNum * frame.length + i)
@@ -102,7 +84,7 @@ public class ImpulseStreamModule implements StreamModule {
 				}
 			}
 		}
-		*/
+		
 		ImpulseFrame impulseFrame = header.new ImpulseFrame(peakOffsets,
 				peakMagnitudes);
 		return impulseFrame;
@@ -114,7 +96,8 @@ public class ImpulseStreamModule implements StreamModule {
 		int i = 0;
 		int peakNum = 0;
 		while (i < frameSample) {
-			double value = java.lang.Math.abs((double) frame[start_index + i]) / 65535.0;
+			double value = java.lang.Math.abs((double) frame[start_index + i]) / 32768.0;
+			
 			if (value >= threshold) {
 				peakNum++;
 			}
@@ -126,7 +109,9 @@ public class ImpulseStreamModule implements StreamModule {
 		if (max > this.max) {
 			// adaptThreshold(max);
 		}
+		
 		return peakNum;
+		
 	}
 
 	private void adaptThreshold(double maxH) {
--- test_all.sh
+++ test_all.sh
@@ -20,7 +20,7 @@ cd test/
 #
 
 java $OPTS $PACKAGE.stream.RawAudioFileStream mangled_audio.wav sample_input-1.wav
-#java $OPTS $PACKAGE.module.FeatureExtractTest impulses-1.txt sample_input-1.wav 
+#java $OPTS $PACKAGE.module.ImpulseStreamModule impulses-1.txt sample_input-1.wav 
 java $OPTS $PACKAGE.module.ConsolidateModule i 1-1 impulses-c.txt impulses-1.txt 
 java $OPTS $PACKAGE.module.TDOACorrelationModule distances.txt impulses-1.txt impulses-2.txt 
 java $OPTS $PACKAGE.module.ConsolidateModule d 1-1 distance-c.txt distance-1.txt 
