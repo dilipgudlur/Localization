055ac4e4e4fae90b50920b6ccc0d917c9d50b3ef
Qi Zheng <zqsharejoy@gmail.com>
2011-11-24 02:50:55 -0800

    use rms to detect peak

--- src/edu/cmu/pandaa/module/ImpulseStreamModule.java
+++ src/edu/cmu/pandaa/module/ImpulseStreamModule.java
@@ -9,57 +9,68 @@ import edu.cmu.pandaa.header.StreamHeader.StreamFrame;
 import edu.cmu.pandaa.stream.ImpulseFileStream;
 import edu.cmu.pandaa.stream.RawAudioFileStream;
 
+import java.util.ArrayList;
+
+import sun.security.krb5.internal.SeqNumber;
+
 public class ImpulseStreamModule implements StreamModule {
-
-	private int sampleRate;
   private double usPerSample;
-	private double thd; // threshold for peak
-	private double thdPeak = 0.4; // general threshold for peak
-	private ImpulseHeader header;
-	int numSilence = 200; // find the silence period (200 samples long)
-	int numFindPeak = 50; // find the highest peak among 50 samples
-	static double preRms = Double.MAX_VALUE;
-	double div = 4;
-	static boolean skipFrame = false;
+  private final int slowWindow = 1000;
+  private final int fastWindow = 50;
+  private final double jerk = 4;
+  private final double base = 50;
+  private ImpulseHeader header;
+  boolean prevPeak = false; // start with peak supression turned on
 
-	public ImpulseStreamModule() {
-	}
+  public ImpulseStreamModule() {
+  }
 
-	public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
-		int arg = 0, loops = 1;
-		String impulseFilename = args[arg++];
-		String audioFilename = args[arg++];
-		if (args.length > arg || args.length < arg) {
-			throw new IllegalArgumentException("Invalid number of arguments");
-		}
+    int arg = 0, loops = 1;
+    String impulseFilename = args[arg++];
+    String audioFilename = args[arg++];
+    if (args.length > arg || args.length < arg) {
+      throw new IllegalArgumentException("Invalid number of arguments");
+    }
 
-		System.out.println("ImpulseStream: " + impulseFilename + " "
-				+ audioFilename);
+    System.out.println("FeatureStream: " + impulseFilename + " "
+            + audioFilename);
 
-		RawAudioFileStream rfs = new RawAudioFileStream(audioFilename);
-		ImpulseFileStream foo = new ImpulseFileStream(impulseFilename, true);
-		ImpulseStreamModule ism = new ImpulseStreamModule();
-		RawAudioFileStream rfso = new RawAudioFileStream(impulseFilename
-				+ ".wav", true);
+    RawAudioFileStream rfs = new RawAudioFileStream(audioFilename);
+    ImpulseFileStream foo = new ImpulseFileStream(impulseFilename, true);
+    ImpulseStreamModule ism = new ImpulseStreamModule();
+    RawAudioFileStream rfso = new RawAudioFileStream(impulseFilename
+            + ".wav", true);
 
-		RawAudioHeader header = (RawAudioHeader) rfs.getHeader();
-		rfso.setHeader(header);
-		ImpulseHeader iHeader = (ImpulseHeader) ism.init(header);
-		foo.setHeader(iHeader);
+    RawAudioHeader header = (RawAudioHeader) rfs.getHeader();
+    rfso.setHeader(header);
+    ImpulseHeader iHeader = (ImpulseHeader) ism.init(header);
+    foo.setHeader(iHeader);
 
-		RawAudioFrame audioFrame = null;
-		while ((audioFrame = (RawAudioFrame) rfs.recvFrame()) != null) {
-			ImpulseFrame streamFrame = ism.process(audioFrame);
-			foo.sendFrame(streamFrame);
-			ism.augmentAudio(audioFrame, streamFrame);
-			rfso.sendFrame(audioFrame);
-		}
-		rfso.close();
-		foo.close();
-	}
+    RawAudioFrame audioFrame = null;
+    while ((audioFrame = (RawAudioFrame) rfs.recvFrame()) != null) {
+      ImpulseFrame streamFrame = ism.process(audioFrame);
+      foo.sendFrame(streamFrame);
+      ism.augmentAudio(audioFrame, streamFrame);
+      rfso.sendFrame(audioFrame);
+    }
+    rfso.close();
+    foo.close();
+  }
 
-	private void augmentAudio(RawAudioFrame audio, ImpulseFrame impulses) {
+  private void augmentAudio(RawAudioFrame audio, ImpulseFrame impulses) {
+    //for (int i = 0; i < audio.audioData.length; i++) {
+    //  audio.audioData[i] = (short) (audio.audioData[i] / 2);
+    //}
+    for (int i = 0; i < impulses.peakOffsets.length; i++) {
+      audio.audioData[timeToSampleOffset(impulses.peakOffsets[i])] = Short.MAX_VALUE;
+      //System.out.println(timeToSampleOffset(impulses.peakOffsets[i]));
+    }
+     //audio.audioData[1715] = Short.MAX_VALUE;
+  }
+
+	private void augmentAudioP(RawAudioFrame audio, ImpulseFrame impulses) {
 		for (int i = 0; i < audio.audioData.length; i++) {
 			audio.audioData[i] = (short) (audio.audioData[i] / 2);
 		}
@@ -68,189 +79,119 @@ public class ImpulseStreamModule implements StreamModule {
 		}
 		// audio.audioData[0] = Short.MAX_VALUE;
 	}
+	
+  @Override
+  public StreamHeader init(StreamHeader inHeader) {
+    if (!(inHeader instanceof RawAudioHeader))
+      throw new RuntimeException("Wrong header type");
+    RawAudioHeader rah = (RawAudioHeader) inHeader;
+    rah.initFilters(30, 0);
+    int sampleRate = (int) rah.getSamplingRate();
+    usPerSample = Math.pow(10,6) / (double) sampleRate; // us per sample
+    header = new ImpulseHeader(inHeader.id, inHeader.startTime,
+            inHeader.frameTime);
+    return header;
+  }
 
-	@Override
-	public StreamHeader init(StreamHeader inHeader) {
-		if (!(inHeader instanceof RawAudioHeader))
-			throw new RuntimeException("Wrong header type");
-		int sr = (int) ((RawAudioHeader) inHeader).getSamplingRate();
-		if (sr != 0) {
-			sampleRate = sr;
+  @Override
+  public ImpulseFrame process(StreamFrame inFrame) {
+    int peaks = 0;
+    ArrayList<Integer> peakOffsets = new ArrayList<Integer>(1);
+    ArrayList<Short> peakMagnitudes = new ArrayList<Short>(1);
+
+    if (!(inFrame instanceof RawAudioFrame))
+      throw new RuntimeException("Wrong frame type");
+
+    RawAudioFrame raf = (RawAudioFrame) inFrame;
+    double[] slowFrame = raf.smooth(slowWindow);
+    double[] fastFrame = raf.smooth(fastWindow);
+    for (int j=1700;j<1750;j++)
+    {
+    	 //System.out.println(fastFrame[j]);
+    }
+    //System.out.println("finish");
+    int index_fast = maxDiv(fastFrame, slowFrame);
+    short[] data = raf.getAudioData();
+    //System.out.println("prevPeak of frame "+ raf.seqNum + " "+prevPeak);
+    if (!prevPeak && index_fast != -1)
+    {
+    	peakOffsets.add(sampleToTimeOffset(index_fast));
+        peakMagnitudes.add((short) fastFrame[index_fast]);
+        //System.out.println("index_slow: "+index_slow);
+        prevPeak = true;
+    }
+    else
+    	prevPeak = false;
+    for (int i = 0; i < data.length;i ++) {
+        double slow = slowFrame[i];
+        double fast = fastFrame[i];
+        double tmp = (i%2 == 0) ? -fast : slow;
+        data[i] = (short) tmp;
+        //data[i] = (short) fast;
+      }
+    /*
+    for (int i = 0; i < data.length;i ++) {
+      double slow = slowFrame[i];
+      double fast = fastFrame[i];
+      
+      if (fast > (slow*jerk + base)) {
+        if (!prevPeak) {
+          System.out.println("" + fast + " " + slow);
+          peakOffsets.add(sampleToTimeOffset(i));
+          peakMagnitudes.add((short) fast);
+          for (int jj=0;jj<150;jj++)
+          {
+          	System.out.println(slowFrame[jj]);
+          }
+        }
+        prevPeak = true;
+        
+      } else {
+        prevPeak = false;
+      }
+
+      double tmp = (i%2 == 0) ? -fast : slow;
+      data[i] = (short) tmp;
+      //data[i] = (short) fast;
+    }
+*/
+    return header.new ImpulseFrame(peakOffsets, peakMagnitudes);
+  }
+
+
+  private int maxDiv(double[] fastFrame, double[] slowFrame) {
+	  double[] div = new double[fastFrame.length-1];
+	  double max = 0;
+	  int index = -1;
+	for(int j = 2; j<fastFrame.length;j++)
+	{
+		div[j-2] = fastFrame[j-1]-fastFrame[j-2];
+		if(div[j-2]> max && fastFrame[j] > (slowFrame[j]*jerk + base))
+		{
+			max = div[j-2];
+			index = j;
 		}
-		usPerSample = Math.pow(10,6) / (double) sampleRate; // us per sample
-		header = new ImpulseHeader(inHeader.id, inHeader.startTime,
-				inHeader.frameTime);
-		return header;
 	}
-
-	@Override
-	public ImpulseFrame process(StreamFrame inFrame) {
-		if (!(inFrame instanceof RawAudioFrame))
-			throw new RuntimeException("Wrong frame type");
-
-		short[] frame = ((RawAudioFrame) inFrame).getAudioData();
-		double[] frameD = short2double(frame);
-		int numMaxPeaks = 1;
-		int iPeaks = 0;
-		int[] peakLocations = new int[numMaxPeaks];
-		double MaxHeight;
-		int HeightLocation;
-
-		int ii = 0;
-		double sum, mean, rms;
-		thd = div * preRms;
-
-		// find the first peak
-		while (skipFrame == false && ii < numSilence) {
-			if (frameD[ii] > thd && frameD[ii] > thdPeak && isPeak(frame, ii)) {
-				HeightLocation = ii;
-				peakLocations[iPeaks++] = HeightLocation;
-				preRms = Double.MAX_VALUE;
-				break;
-			}
-			ii++;
+	if(index != -1)
+	{
+		for (int jj = index - 5; jj< index+20;jj++)
+		{
+			//System.out.println(fastFrame[jj]);
 		}
-
-		// find the highest peak among numFindPeak
-		/*
-		MaxHeight = 0;
-		HeightLocation = 0;
-		while (skipFrame == false && ii < numSilence) {
-			for (ii = 0; ii < numSilence; ii++) {
-				if (frameD[ii] > thd && frameD[ii] > thdPeak
-						&& isPeak(frame, ii) && frameD[ii] > MaxHeight) {
-					HeightLocation = ii;
-					MaxHeight = frameD[ii];
-				}
-			}
-			if (MaxHeight != 0) {
-				peakLocations[iPeaks++] = HeightLocation;
-				preRms = Double.MAX_VALUE;
-			}
-		}
-		*/
-		ii = numSilence;
-		while (skipFrame == false && iPeaks < numMaxPeaks && ii < frameD.length) {
-			sum = 0.0;
-			for (int jj = 0; jj < numSilence; jj++) {
-				sum += frameD[ii - jj - 1] * frameD[ii - jj - 1];
-			}
-			mean = sum / numSilence;
-			rms = java.lang.Math.sqrt(mean);
-			thd = div * rms;
-
-			if (ii == frameD.length - 1) {
-				preRms = rms;
-			}
-
-			if (frameD[ii] > thd && frameD[ii] > thdPeak) {
-				MaxHeight = frameD[ii];
-				HeightLocation = ii;
-				for (int nextSample = 0; nextSample < numFindPeak; nextSample++) {
-					if (nextSample + ii == frameD.length)
-						break;
-					else if (MaxHeight < frameD[nextSample + ii]
-							&& isPeak(frame, nextSample + ii)) {
-						MaxHeight = frameD[nextSample + ii];
-						HeightLocation = nextSample + ii;
-					}
-				}
-				peakLocations[iPeaks++] = HeightLocation;
-				if (iPeaks == numMaxPeaks)
-					break;
-				ii = HeightLocation + 1;
-			} else {
-				ii++;
-			}
-		}
-
-		if (preRms == Double.MAX_VALUE) {
-			ii = frame.length;
-			sum = 0.0;
-			for (int jj = 0; jj < numSilence; jj++) {
-				sum += frameD[ii - jj - 1] * frameD[ii - jj - 1];
-			}
-			mean = sum / numSilence;
-			rms = java.lang.Math.sqrt(mean);
-			preRms = rms;
-		}
-
-		if (iPeaks > 0) {
-			skipFrame = true;
-			preRms = Double.MAX_VALUE;
-		} else
-			skipFrame = false;
-		short[] peakMagnitudes = new short[iPeaks];
-		int[] peakOffsets = new int[iPeaks];
-
-		for (int i = 0; i < iPeaks; i++) {
-      short peak = frame[peakLocations[i]];
-      if (peak == Short.MIN_VALUE) // can't invert Short.MIN_VALUE!
-        peak++;
-			peakMagnitudes[i] = peak < 0 ? (short) -peak : peak;
-			peakOffsets[i] = sampleToTimeOffset(peakLocations[i]);
-		}
-
-		ImpulseFrame impulseFrame = header.new ImpulseFrame(peakOffsets,
-				peakMagnitudes);
-		return impulseFrame;
+		System.out.println("index: "+index + " max: "+max);
 	}
+	
+	return index;
+}
 
-	private double[] short2double(short[] frame) {
-		double[] frameD = new double[frame.length];
-		for (int i = 0; i < frame.length; i++) {
-			frameD[i] = java.lang.Math.abs((double) frame[i]) / 32768.0;
-		}
-		return frameD;
-	}
+private int sampleToTimeOffset(int sample) {
+    return (int) (sample * usPerSample);
+  }
 
-	private int sampleToTimeOffset(int sample) {
-		return (int) (sample * usPerSample);
-	}
+  private int timeToSampleOffset(int time) {
+    return (int) ((double) time / usPerSample);
+  }
 
-	private int timeToSampleOffset(int time) {
-		return (int) ((double) time / usPerSample);
-	}
-
-	/*
-	 * Check whether there are lower points around the sample.
-	 */
-	private boolean isPeak(short[] frame, int i) {
-		if (i == frame.length - 1) {
-			if (frame[i] > 0 && frame[i] > frame[i - 1]) {
-				return true;
-			}
-			if (frame[i] < 0 && frame[i] < frame[i - 1]) {
-				return true;
-			}
-			return false;
-		} else if (i == 0) {
-			if (frame[i] > 0 && frame[i] > frame[i + 1]) {
-				return true;
-			}
-			if (frame[i] < 0 && frame[i] < frame[i + 1]) {
-				return true;
-			}
-			return false;
-		} else {
-			if (frame[i] > 0 && frame[i] > frame[i - 1]
-					&& frame[i] > frame[i + 1]) {
-				return true;
-			}
-			if (frame[i] < 0 && frame[i] < frame[i - 1]
-					&& frame[i] < frame[i + 1]) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public void setThreshold(double thr, double thrP) {
-		thd = thr;
-		thdPeak = thrP;
-	}
-
-	public void close() {
-	}
-
+  public void close() {
+  }
 }
