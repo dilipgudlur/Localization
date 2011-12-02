68e3c9058e0d83b4cfd2c024514777e5a33433dd
Trevor Pering <grafnu@gmail.com>
2011-12-01 21:12:02 -0800

    Updating app pipeline and a bunch of other minor things

--- src/edu/cmu/pandaa/framework/SinglePipeline.java
+++ src/edu/cmu/pandaa/framework/SinglePipeline.java
@@ -1,5 +1,6 @@
 package edu.cmu.pandaa.framework;
 
+import com.sun.xml.internal.bind.v2.model.core.ID;
 import edu.cmu.pandaa.header.ImpulseHeader;
 import edu.cmu.pandaa.header.RawAudioHeader;
 import edu.cmu.pandaa.header.StreamHeader;
@@ -7,6 +8,8 @@ import edu.cmu.pandaa.header.StreamHeader.StreamFrame;
 import edu.cmu.pandaa.module.ConsolidateModule;
 import edu.cmu.pandaa.module.ImpulseStreamModule;
 import edu.cmu.pandaa.module.StreamModule;
+import edu.cmu.pandaa.stream.FileStream;
+import edu.cmu.pandaa.stream.ImpulseFileStream;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,8 +26,10 @@ public class SinglePipeline implements StreamModule {
   /* Then we consolidate bunches of impulse frames into larger consolidated frames for processing */
   StreamModule consolidate = new ConsolidateModule('i', 1, 1, 1, 1);
 
+  FileStream trace;
+
   @Override
-  public StreamHeader init(StreamHeader inHeader) {
+  public StreamHeader init(StreamHeader inHeader) throws Exception {
     if (!(inHeader instanceof RawAudioHeader)) {
       throw new IllegalArgumentException("Requires RawAudioHeader");
     }
@@ -33,20 +38,29 @@ public class SinglePipeline implements StreamModule {
     if (!(header instanceof ImpulseHeader)) {
       throw new IllegalArgumentException("Output should be ImpulseHeader");
     }
+
+    trace = new ImpulseFileStream(inHeader.id + ".txt", true);
+    trace.setHeader(header);
+
     return header;
   }
 
   @Override
-  public StreamFrame process(StreamFrame inFrame) {
+  public StreamFrame process(StreamFrame inFrame) throws Exception {
     if (inFrame == null) {
       return null;
     }
+
     StreamFrame frame = impulse.process(inFrame);
     frame = consolidate.process(frame);
+
+    trace.sendFrame(frame);
+
     return frame;
   }
 
   @Override
   public void close() {
+    trace.close();
   }
 }
