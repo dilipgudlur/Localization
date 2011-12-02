513cf1ea8a21d4c37130ca0177f8728dfd0f7d1d
dilipgudlur <dilip.gudlur@sv.cmu.edu>
2011-12-02 14:40:57 -0800

    update RMSModule

--- src/edu/cmu/pandaa/module/RMSModule.java
+++ src/edu/cmu/pandaa/module/RMSModule.java
@@ -1,9 +1,59 @@
 package edu.cmu.pandaa.module;
 
+import mdsj.MDSJ;
+import edu.cmu.pandaa.header.DistanceHeader;
+import edu.cmu.pandaa.header.GeometryHeader;
+import edu.cmu.pandaa.header.StreamHeader;
+import edu.cmu.pandaa.header.GeometryHeader.GeometryFrame;
+import edu.cmu.pandaa.header.DistanceHeader.DistanceFrame;
+import edu.cmu.pandaa.header.StreamHeader.StreamFrame;
+import edu.cmu.pandaa.stream.DistanceFileStream;
+import edu.cmu.pandaa.stream.FrameStream;
 import edu.cmu.pandaa.stream.GeometryFileStream;
-import edu.cmu.pandaa.stream.RMSFileStream;
 
-public class RMSModule {
+
+public class RMSModule implements StreamModule{
+	DistanceHeader dOut;
+	
+	public void runModule(FrameStream inGeometryStream, FrameStream outDistanceStream) throws Exception {
+	    try{
+	      StreamHeader header = init(inGeometryStream.getHeader());
+	      outDistanceStream.setHeader(header);
+	      StreamFrame frameIn,frameOut;
+	      while ((frameIn = inGeometryStream.recvFrame()) != null) {
+	        frameOut = process(frameIn);
+	        outDistanceStream.sendFrame(frameOut);
+	      }
+	    }catch(Exception e){
+	      e.printStackTrace();
+	    }
+	    outDistanceStream.close();
+	  }
+	
+	public StreamHeader init(StreamHeader inHeader) {
+	    if (!(inHeader instanceof GeometryHeader))
+	      throw new RuntimeException("Wrong header type");
+
+	    GeometryHeader hIn = (GeometryHeader)inHeader ;
+	    String id = "";
+	    for(int i = 0; i < hIn.deviceIds.length; i++)
+	    {
+	    	id += hIn.deviceIds[i]; 
+	    } 
+	    dOut = new DistanceHeader(id, hIn.startTime, hIn.frameTime, hIn.deviceIds);
+	    return dOut;
+	  }
+	
+	public StreamFrame process(StreamFrame inFrame) {
+	    if (!(inFrame instanceof GeometryFrame))
+	      throw new RuntimeException("Wrong frame type");
+	    GeometryFrame gfIn = (GeometryFrame) inFrame ;
+	    double[] x=null,y=null;
+	    
+	    DistanceFrame dfOut = dOut.makeFrame(x,y);
+	    return dfOut ;
+	  }
+
 	
 	public static void main(String[] args) throws Exception
 	{
@@ -17,11 +67,16 @@ public class RMSModule {
 		
 	    System.out.println("RMS: " + outArg + " " + inArg1 + " " + inArg2);
 	    
-	    RMSFileStream rIn1 = new RMSFileStream(inArg1);
-	    RMSFileStream rIn2 = new RMSFileStream(inArg2);
-	    RMSFileStream rOut = new RMSFileStream(outArg, true);
+	    GeometryFileStream rIn1 = new GeometryFileStream(inArg1);
+	    GeometryFileStream rIn2 = new GeometryFileStream(inArg2);
+	    DistanceFileStream rOut = new DistanceFileStream(outArg, true);
 
 	    
+	}
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
