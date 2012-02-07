ac27046eb86c5147797b6b8de314b86f956c642c
Trevor Pering <grafnu@gmail.com>
2012-02-06 19:42:18 -0800

    Adding streaming script

---
+++ stream.sh
@@ -0,0 +1,15 @@
+JAR=Localization.jar
+CLASSPATH=$PWD/`find . -name $JAR`
+
+if [ ! -f $CLASSPATH ]; then
+  echo Could not find $JAR
+  exit
+fi
+
+CLASSPATH=$CLASSPATH:$PWD/lib/mdsj.jar
+#echo CLASSPATH is $CLASSPATH
+OPTS="-classpath $CLASSPATH" #use with Linux
+#OPTS="-classpath `cygpath -wp $CLASSPATH`" #use with Cygwin ons Windows
+PACKAGE=edu.cmu.pandaa
+
+java -cp $CLASSPATH $PACKAGE.framework.App local
