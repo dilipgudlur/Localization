016814f3494e884ae4626cbb16cfb4fa4d9a9555
Trevor Pering <grafnu@gmail.com>
2012-03-18 10:09:36 -0700

    Adding traceit script

---
+++ traceit.sh
@@ -0,0 +1,16 @@
+CLASSPATH=Localization.jar
+CLASSPATH=$CLASSPATH:$PWD/lib/mdsj.jar
+
+if [ "$@" == "" ]; then
+  files=capture_*
+else
+  files="$@"
+fi
+
+for file in $files; do
+  TARGET=trace-$file
+  rm -rf trace $TARGET
+  java -cp $CLASSPATH edu.cmu.pandaa.framework.App $file
+  mv trace $TARGET
+done
+
